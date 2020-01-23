7be14a87200147919267ce1280c99d6bf1480d77
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-01-23 09:15:22 +0100

    Add queries and mutations to schema

--- packages/expense_tracker/src/schema/index.js
+++ packages/expense_tracker/src/schema/index.js
@@ -1,4 +1,4 @@
-const { GraphQLInt, GraphQLString, GraphQLObjectType, GraphQLList, GraphQLSchema } = require('graphql');
+const { GraphQLID, GraphQLInt, GraphQLNonNull, GraphQLString, GraphQLObjectType, GraphQLList, GraphQLSchema } = require('graphql');
 const Budget = require('../models/Budget');
 const Category = require('../models/Category');
 const Transaction = require('../models/Transaction');
@@ -83,7 +83,83 @@ const schema = new GraphQLSchema({
         },
         resolve: (root, args) => {
           const category = new Category(args);
-          return category.save(args)
+          return category.save(function (err) {
+            if (err) return { code: 400, message: 'Category could not be saved.' };
+            return { code: 200, message: 'Category saved.' };
+          });
+        }
+      },
+      createTransaction: {
+        type: TransactionType,
+        args: {
+          description: { type: GraphQLString },
+          amount: { type: GraphQLInt },
+          category: { type: GraphQLNonNull(GraphQLID) }
+        },
+        resolve: (root, args) => {
+          const transaction = new Transaction(args);
+          return transaction.save(function (err) {
+            if (err) return { code: 400, message: 'Transaction could not be saved.' };
+            return { code: 200, message: 'Transaction saved.' };
+          });
+        }
+      },
+      createBudget: {
+        type: BudgetType,
+        args: {
+          amount: { type: GraphQLInt },
+          category: { type: GraphQLNonNull(GraphQLID) }
+        },
+        resolve: (root, args) => {
+          const budget = new Budget(args);
+          return budget.save(function (err) {
+            if (err) return { code: 400, message: 'Budget could not be saved.' };
+            return { code: 200, message: 'Budget saved.' };
+          });
+        }
+      },
+      editCategory: {
+        type: CategoryType,
+        args: {
+          _id: { type: GraphQLNonNull(GraphQLID) },
+          title: { type: GraphQLString }
+        },
+        resolve: (root, args) => {
+          return Category.updateOne(args, function (error, res) {
+            if (error) return { code: 400, message: 'Category could not be updated.' };
+            return { code: 200, message: 'Category saved.' };
+          });
+        }
+      },
+      editTransaction: {
+        type: TransactionType,
+        args: {
+          _id: { type: GraphQLNonNull(GraphQLID) },
+          description: { type: GraphQLString },
+          amount: { type: GraphQLInt },
+          year: { type: GraphQLInt },
+          month: { type: GraphQLInt },
+          day: { type: GraphQLInt },
+          category: { type: GraphQLID }
+        },
+        resolve: (root, args) => {
+          return Transaction.updateOne(args, function (error, res) {
+            if (error) return { code: 400, message: 'Transaction could not be updated.' };
+            return { code: 200, message: 'Transaction saved.' };
+          });
+        }
+      },
+      editBudget: {
+        type: BudgetType,
+        args: {
+          _id: { type: GraphQLNonNull(GraphQLID) },
+          title: { type: GraphQLString }
+        },
+        resolve: (root, args) => {
+          return Budget.updateOne(args, function (error, res) {
+            if (error) return { code: 400, message: 'Budget could not be updated.' };
+            return { code: 200, message: 'Budget saved.' };
+          });
         }
       }
     }
