712edb9d6a7eec3127b889e528b781ff073c47fa
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-02-01 06:11:20 +0100

    Code refactor and cleanup

--- packages/poco_a_poco/src/schema/index.js
+++ packages/poco_a_poco/src/schema/index.js
@@ -1,7 +1,8 @@
-const { GraphQLList, GraphQLSchema, GraphQLString, GraphQLObjectType } = require('graphql');
+const { GraphQLBoolean, GraphQLList, GraphQLSchema, GraphQLString, GraphQLObjectType } = require('graphql');
 const Comment = require('../models/Comment');
 const Objective = require('../models/Objective');
 const Status = require('../models/Status');
+const KeyResult = require('../models/KeyResult');
 
 const StatusType = new GraphQLObjectType({
   name: 'Status',
@@ -19,6 +20,7 @@ const ObjectiveType = new GraphQLObjectType({
     title: { type: GraphQLString },
     description: { type: GraphQLString },
     status: { type: StatusType },
+    dueDate: { type: GraphQLString },
     createdAt: { type: GraphQLString }
   }
 });
@@ -29,6 +31,17 @@ const CommentType = new GraphQLObjectType({
     _id: { type: GraphQLString },
     body: { type: GraphQLString },
     objective: { type: ObjectiveType },
+    createdAt: { type: GraphQLString }
+  }
+});
+
+const KeyResultType = new GraphQLObjectType({
+  name: 'KeyResult',
+  fields: {
+    _id: { type: GraphQLString },
+    description: { type: GraphQLString },
+    objective: { type: ObjectiveType },
+    done: { type: GraphQLBoolean },
     createdAt: { type: GraphQLString }
   }
 });
@@ -46,14 +59,39 @@ const schema = new GraphQLSchema({
       objectives: {
         type: GraphQLList(ObjectiveType),
         resolve: () => {
-          return Objective.find().exec();
+          return Objective.find().populate('status').exec();
         }
       },
       comments: {
         type: GraphQLList(CommentType),
         resolve: () => {
-          return Comment.find().exec()
+          return Comment.find().populate('objective').exec()
         }
+      },
+      keyResults: {
+        type: GraphQLList(KeyResultType),
+        resolve: () => {
+          return KeyResult.find().populate('objective').exec()
+        }
+      }
+    }
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'Mutation',
+    createObjective: {
+      type: ObjectiveType,
+      args: {
+        title: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: StatusType },
+        dueDate: { type: GraphQLString }
+      },
+      resolve: (root, args) => {
+        const objective = Objective(args);
+        return objective.save(function (err) {
+          if (err) return { code: 400, message: 'Objective could not be saved.' };
+          return { code: 200, message: 'Objective saved.' };
+        });
       }
     }
   })
